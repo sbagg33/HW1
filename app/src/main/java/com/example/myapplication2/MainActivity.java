8bcaaa352db034e2a001e3d14931c50a8a28430f
HumbertoBeto <humberto29202@gmail.com>
2020-09-19 20:27:30 -0700

    Added Sabrina's sprint1 code to Beto's sprint 1 code, input array is now sorted using Sabrina's bubble sort algorithm and dispalyed on the screen on successful input

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -9,10 +9,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     public static String numArray[]; //array to hold input
+    public static int numbersIntArray[]; //arrayt to hold integers
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
     //called when the user taps the send button
     public void sendMessage( View view) {
-       // Intent intent = new Intent(this, DisplayMessageActivity.class);
+
         EditText editText = (EditText) findViewById(R.id.editText); // get text from input
         String message = editText.getText().toString(); //convert to string
 
         TextView textView = findViewById(R.id.textView2); //get view for output text view
         TextView valuesText = findViewById(R.id.digitValueBoolText); //get view for value text view
         TextView sizeText = findViewById(R.id.sizeBoolText); //get view for value text view
+        TextView sortedListText = findViewById(R.id.sortedListFinal); //get view for value text view
+
 
         textView.setText(message); //set the text to show input text
         textView.setVisibility(view.VISIBLE); // make text visible
         System.out.println(message);
 
         numArray = (message.split("\\s")); //split input string by spaces
+
 
         //setup toast attributes
         Context context = getApplicationContext();
@@ -46,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(context, "Error: Please enter number values of 0-9 only", duration).show();
             valuesText.setText("False");
+            sortedListText.setVisibility(view.INVISIBLE); // make text visible
         }
 
         //Check method return and update text for size checking 3-8
@@ -54,13 +61,27 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(context, "Error: Please enter 3 to 8 digits, no more no less", duration).show();
             sizeText.setText("False");
+            sortedListText.setVisibility(view.INVISIBLE); // make text visible
         }
         valuesText.setVisibility((view.VISIBLE));
         sizeText.setVisibility((view.VISIBLE));
 
         //send success toast on valid input
         if(valueCheck(numArray) && sizeLimitCheck(numArray)){
-            Toast.makeText(context, "Valid Input", duration).show();
+            Toast.makeText(context, "Valid Input", duration).show(); //valid input toast
+
+            //make string array into int array
+            numbersIntArray = new int[numArray.length]; //integera array
+            for(int i = 0;i < numArray.length;i++)
+            {
+                numbersIntArray[i] = Integer.parseInt(numArray[i]); //changing type to int and adding to new array
+            }
+            numbersIntArray = bubbleSort(numbersIntArray);
+            System.out.println("Final answer below!");
+            System.out.println(Arrays.toString(numbersIntArray));
+            //print to screen
+            sortedListText.setText(Arrays.toString(numbersIntArray)); //set the text to show input text
+            sortedListText.setVisibility(view.VISIBLE); // make text visible
         }
     }
 
@@ -84,5 +105,33 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true; //all digit in array are digits between 0-9
+    }
+
+    //swap those two numbers here
+    public static void swap(int[] arr, int from, int to){
+        int temp = arr[from];
+        arr[from] = arr[to];
+        arr[to] = temp;
+        //system.out.printf("Iteration %d: %s %n",swapNumber, Arrays.toString(arr)); //bring to front mobile
+    }
+
+    public static int[] bubbleSort(int[] numbers){
+        //Print the unsorted array
+        //System.out.printf("Unsorted array: %s%n", Arrays.toString(numbers));
+        int sIndex = 0;
+        int nValues = numbers.length;
+        int eIndex = nValues - 1;
+
+        while (sIndex < eIndex){
+            for (int index = eIndex; index > sIndex; index--){
+                if (numbers[index] < numbers[index -1])
+                    swap(numbers, index, index - 1);
+                System.out.println(Arrays.toString(numbers));
+            }
+            sIndex++;
+        }
+        //System.out.println(Arrays.toString((numbers)));
+        return numbers;
+        //System.out.println("Solution: %s", Arrays.toString(arr)); //bring to front mobile
     }
 }
